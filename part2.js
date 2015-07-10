ace445c0b097e65155af5062843ef05545e8d71d
TacoScott <withorwithoutgod@gmail.com>
2015-07-09 22:24:14 -0500

    Create part2.js

---
+++ part2.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name       Correct the Injustice Part 2
+// @version    0.1
+// @description  Swifty Dream
+// @include      http*
+// @copyright  2012+, You
+// ==/UserScript==
+
+walk(document.body);
+
+function walk(node) 
+{
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+            if(node.parentElement.tagName.toLowerCase() != "script") {
+                handleText(node);
+            }
+			break;
+	}
+}
+
+function handleText(textNode) {
+    var v = textNode.nodeValue;
+    console.log(v);
+    v = v.replace(/Taylor Swift/gi,"National Treasure Taylor Swift");
+    textNode.nodeValue = v;
+}
+
+
